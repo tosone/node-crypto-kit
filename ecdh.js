d22477edf1293d4b210c0cc426c5caffca4157dc
tosone <i@tosiney.com>
2015-09-18 10:36:21 +0800

    update ecdh.js

--- ecdh.js
+++ ecdh.js
@@ -1,16 +1,21 @@
 var crypto = require('crypto');
 console.time("spendtime");
-var alice = crypto.createECDH('secp256k1');
-var bob = crypto.createECDH('secp256k1');
-alice.generateKeys();
-bob.generateKeys();
-var bobPublicKey = bob.getPublicKey('hex');
-var alicePublicKey = alice.getPublicKey('hex');
-var alicePrivateKey = alice.computeSecret(bobPublicKey, 'hex', 'hex');
-var bobPrivateKey = bob.computeSecret(alicePublicKey, 'hex', 'hex');
-console.log("bobPublicKey:" + bobPublicKey);
-console.log("alicePublicKey:" + alicePublicKey);
-console.log("bobPrivateKey:" + bobPrivateKey);
-console.log("alicePrivateKey:" + alicePrivateKey);
-console.log(bobPrivateKey == alicePrivateKey ? "success" : "error");
-console.timeEnd("spendtime");
+var Curves = crypto.getCurves();
+for(var i in Curves){
+	var alice = crypto.createECDH(Curves[i]);
+	var bob = crypto.createECDH(Curves[i]);
+	alice.generateKeys();
+	bob.generateKeys();
+	var bobPublicKey = bob.getPublicKey('hex');
+	var alicePublicKey = alice.getPublicKey('hex');
+	var alicePrivateKey = alice.computeSecret(bobPublicKey, 'hex', 'hex');
+	var bobPrivateKey = bob.computeSecret(alicePublicKey, 'hex', 'hex');
+	console.log("Curves: " + Curves[i]);
+	console.log("bobPublicKey: " + bobPublicKey);
+	console.log("alicePublicKey: " + alicePublicKey);
+	console.log("bobPrivateKey: " + bobPrivateKey);
+	console.log("alicePrivateKey: " + alicePrivateKey);
+	console.log(bobPrivateKey == alicePrivateKey ? "success" : "error");
+	console.timeEnd("spendtime");
+	console.log();
+}
